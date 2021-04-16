4f3011e8c1022c0171b0d4fa38018de2da90736a
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-16 14:53:28 -0400

    Update UI with city and weather info

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,21 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUI = data => {
+  const { cityDetails, weather } = data;
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  if (card.classList.contains("d-none")) card.classList.remove("d-none");
+};
 
 const updateCity = async city => {
   const cityDetails = await getCity(city);
@@ -13,5 +30,5 @@ cityForm.addEventListener("submit", e => {
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  updateCity(city).then(data => {});
+  updateCity(city).then(data => updateUI(data));
 });
