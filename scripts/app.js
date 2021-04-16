ad4482c619510f49fd54fbb94d37e9cc2c1041f3
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-16 10:57:23 -0400

    Get weather details for searched city

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+const cityForm = document.querySelector("form");
+
+const updateCity = async city => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+
+  return { cityDetails, weather };
+};
+
+cityForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  updateCity(city).then(data => {});
+});
